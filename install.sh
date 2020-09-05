8086d471cadb326bdfc1a2417412c4e9345afddd
EC2 Default User <ec2-user@ip-172-31-7-167.ap-south-1.compute.internal>
2020-09-05 11:40:52 +0000

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
+sudo yum groupinstall "Development Tools"
 sudo yum install git python3 gcc gmp gmp-devel gmp-status python3-devel mpfr-devel libmpc-devel -y
+cd gflags
+git checkout v2.0
+./configure && make && sudo make install
+cd ..
+set CPATH=/usr/local/include
+set LIBRARY_PATH=/usr/local/lib
+sudo yum install snappy snappy-devel
+sudo yum install zlib zlib-devel
+sudo yum install bzip2 bzip2-devel
+sudo yum install lz4-devel
+wget https://github.com/facebook/zstd/archive/v1.1.3.tar.gz
+mv v1.1.3.tar.gz zstd-1.1.3.tar.gz
+tar zxvf zstd-1.1.3.tar.gz
+cd zstd-1.1.3
+make && sudo make install
+cd ..
