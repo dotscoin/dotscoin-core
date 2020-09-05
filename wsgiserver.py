102bdea6a866837640d3b8f398c8691f77903ecf
EC2 Default User <ec2-user@ip-172-31-7-165.ap-south-1.compute.internal>
2020-09-05 11:38:02 +0000

    multithreaded rpc

---
+++ wsgiserver.py
@@ -0,0 +1,19 @@
+
+from wsgiref.simple_server import make_server, WSGIServer
+import socketserver
+
+host='0.0.0.0'
+port=7000
+class ThreadingWSGIServer(socketserver.ThreadingMixIn, WSGIServer):
+    daemon_threads = True
+
+class Server:
+    def __init__(self, wsgi_app, listen=host,port=port):
+        self.wsgi_app = wsgi_app
+        self.listen = listen
+        self.port = port
+        self.server = make_server(self.listen, self.port, self.wsgi_app,
+                                  ThreadingWSGIServer)
+
+    def serve_forever(self):
+        self.server.serve_forever()
