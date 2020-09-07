80d6cdd5218f579463ad61f86e8da1c2fc868444
deadlycoder07 <ramiz.mollah07@outlook.com>
2020-09-07 11:18:37 +0000

    receiver changes

--- server.py
+++ server.py
@@ -9,39 +9,6 @@ from threads.rpc import rpc_receive
 host = '0.0.0.0'
 port = 8080
 
-sel = selectors.DefaultSelector()
-
-def accept_wrapper(sock):
-    conn, addr = sock.accept()
-    print('accepted connection from', addr)
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
-
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print('closing connection to', data.addr)
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print('echoing', repr(data.outb), 'to', data.addr)
-            sent = sock.send(data.outb)
-            data.outb = data.outb[sent:]
-
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((host, port))
-lsock.listen()
-print('listening on', (host, port))
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
 cpucount = os.cpu_count()
 print("total cpu cores in the system=",cpucount)
 if cpucount < 4:
@@ -51,11 +18,3 @@ rpc=multiprocessing.Process(target=rpc_receive)
 rpc.start()
 receiver=multiprocessing.Process(target=broadcast_receive)
 receiver.start()
-
-while True:
-    events = sel.select(timeout=None)
-    for key, mask in events:
-        if key.data is None:
-            accept_wrapper(key.fileobj)
-        else:
-            service_connection(key, mask)
