fe09cfaef68858a9042d17dbd813bf3cb1521ecb
deadlycoder07 <ramiz.mollah07@outlook.com>
2020-09-09 10:31:01 +0000

    rpc fix

--- tests/testreceiver.py
+++ tests/testreceiver.py
@@ -2,4 +2,4 @@ import socket
 
 sock= socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind(('0.0.0.0',6500))
-sock.sendto("hello".encode('utf-8'),('34.122.30.88', 9000))
+sock.sendto("hello".encode('utf-8'),('45.123.160.161', 3500))
