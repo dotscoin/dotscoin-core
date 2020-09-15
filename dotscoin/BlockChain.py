7b07b481695ac491d55952e8c6d79015be996e9d
rishav4101 <rajkumarrishav4101@gmail.com>
2020-09-16 00:22:51 +0530

    pickle added

--- dotscoin/BlockChain.py
+++ dotscoin/BlockChain.py
@@ -1,20 +1,31 @@
+# all imports
 import redis
 import json
 from typing import List
 from dotscoin.Block import Block
 
 class BlockChain:
+    """ Object Initialization """
+
     data = ""
 
     def __init__(self):
+        """ Initialization """
+
         self.blocks: List[Block] = []
         self.redis_client = redis.Redis(host='localhost', port=6379, db=0)
         
     def add_block(self, blk):
+        """ Adding Block to blockchain """
+
         self.redis_client.rpush('chain', json.dumps(blk))
 
     def read_chain(self, index: int):
+        """ Reading the blockchain """
+
         return self.redis_client.lrange('chain', 0, index)
 
     def clear_chain(self):
+        """ Clearing the blockchain """
+        
         self.redis_client.flushdb()
